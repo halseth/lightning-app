948a3dc40ba39f4d608b90bd36c00025014bd8a0
Johan T. Halseth <johanth@gmail.com>
2018-11-06 09:35:06 +0100

    add JS protos

---
+++ mobile/google/api/annotations_pb.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global = Function('return this')();
